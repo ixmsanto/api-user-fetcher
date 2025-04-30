0b42ca5fcd814f6d280bb7a77573999f17869d73
ixmsanto <santhoshjeje@gmail.com>
2025-04-30 13:19:10 +0530

    feat: routes created for feat-users

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class, 'index'])->name('users.index');
+Route::get('/fetch-users', [UserController::class, 'fetchAndStoreUsers'])->name('users.fetch');
